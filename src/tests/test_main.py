e70971b82dd3afc30a156e7f364693c9a674ecc6
Yurii Momotenko <momotenko.yuriy@gmail.com>
2024-03-16 17:03:31 +0100

    Add mock test

---
+++ src/tests/test_main.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestFunctionToTest(unittest.TestCase):
+    def test_function_to_test(self):
+        # Add a simple test that always passes
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()
